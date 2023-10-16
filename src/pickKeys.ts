d910035f79c4b4f01e36540a2610e0d22aa17c6b
amochkin <a@mochkin.com>
2023-10-16 12:26:35 +0200

    chore: var refactor

--- src/pickKeys.ts
+++ src/pickKeys.ts
@@ -6,7 +6,7 @@ const DEFAULT_SEPARATOR = '.';
 /**
  * Picks selected properties from the object by dot separated path, i.e. 'a.b.c'
  * @param obj Original object
- * @param path Property path as dot separated string, or array of paths. Example: 'a.b.c' or ['a.b.c', 'a.b.d']
+ * @param pathOrPathArray Property path as dot separated string, or array of paths. Example: 'a.b.c' or ['a.b.c', 'a.b.d']
  * @return obj Object with picked properties
  * @example
  * pickKeys({ a: { b: { c: 1, d: 2 } } }, 'a.b.c')
@@ -14,10 +14,10 @@ const DEFAULT_SEPARATOR = '.';
  * pickKeys({ a: { b: { c: 1, d: 2, e: 3 } } }, ['a.b.c', 'a.b.d'])
  * // returns { a: { b: { c: 1, d: 2 } } }
  */
-export const pickKeys = <T extends object>(obj: T, path: TypeNestedKeyOf<T>[] | TypeNestedKeyOf<T>) => {
+export const pickKeys = <T extends object>(obj: T, pathOrPathArray: TypeNestedKeyOf<T>[] | TypeNestedKeyOf<T>) => {
 	const accumulator = {};
 
-	(Array.isArray(path) ? path : [String(path)]).forEach((propPath: string) => {
+	(Array.isArray(pathOrPathArray) ? pathOrPathArray : [String(pathOrPathArray)]).forEach((propPath: string) => {
 		const pathArray = propPath.split(DEFAULT_SEPARATOR);
 
 		const value = getValueByPath(obj, pathArray);
